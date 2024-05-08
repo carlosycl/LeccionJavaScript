5bbbea8b4581078c0e3721913d165a70a3cf30b2
carlosycl <cycortes1701@gmail.com>
2024-05-07 22:34:54 -0500

    Tipos de datos JS

---
+++ TiposDeDatos.js
@@ -0,0 +1,20 @@
+/*
+Tipos de datos en 
+javaScript
+*/
+
+//Tipo de dato String
+var nombre = "carlos";
+console.log(nombre);
+
+//Tipo de dato numero
+var numero = 1000;
+console.log(numero);
+
+//Tipo de dato object
+var objeto = {
+    nombre : "yesid", 
+    telefono : 12345,
+    apellido : "cortes" 
+}
+console.log(objeto)
