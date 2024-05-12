e9352052219241ad26d34a1133fd7e4643c2783d
carlosycl <cycortes1701@gmail.com>
2024-05-11 23:32:52 -0500

    Operadores Aritmeticos

---
+++ OperadoresAritmeticos.js
@@ -0,0 +1,18 @@
+let a=3, b=2;
+
+z = a+b;
+console.log("El resultado de la suma es: " + z);
+
+z= a*b;
+console.log("El restultado de la resta es: " + z);
+
+z= a-b;
+console.log("El resultado de la resta es: " + z);
+
+z= a**b;
+console.log("El restultado del exponente es: " + z);
+
+//resuido 
+
+z= a%b;
+console.log("El residuo entre 3 y 2 es: " + z);
