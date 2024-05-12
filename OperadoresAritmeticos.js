498ef35ed2ed9d2a3e50b117312839a84649050b
carlosycl <cycortes1701@gmail.com>
2024-05-11 23:47:37 -0500

    pre y post decremento e incremento

--- OperadoresAritmeticos.js
+++ OperadoresAritmeticos.js
@@ -1,6 +1,6 @@
 let a=3, b=2;
 
-z = a+b;
+let z = a+b;
 console.log("El resultado de la suma es: " + z);
 
 z= a*b;
@@ -16,3 +16,28 @@ console.log("El restultado del exponente es: " + z);
 
 z= a%b;
 console.log("El residuo entre 3 y 2 es: " + z);
+
+//Incremento 
+//pre-incremento (El operador "++" antes de la variable) 
+
+z= ++a;
+console.log(a);
+console.log(z);
+
+//Post-incremento (El operador "++" despues de la variable)
+
+z= a++;
+console.log(a);
+console.log(z);
+
+//pre-decremento 
+z = --a;
+console.log(a);
+console.log(z);
+
+// post-decremento
+z = a--;
+console.log(a);
+console.log(z);
+
+
