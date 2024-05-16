039c8894ad1a3be2eb11117efe456d77245f114c
carlosycl <cycortes1701@gmail.com>
2024-05-15 21:27:41 -0500

    Operadores de igualdad

---
+++ OperadoresIgualacion.js
@@ -0,0 +1,12 @@
+let a = 1, b = 2, c = 3, d = "1", e = 2;
+let z = a == b; 
+let y = a == d; //Operador de igualdad sin importar el tipo  
+
+console.log(z)
+console.log(y)
+
+let x = b === e;
+let w = a === d; //Operador de igualdad evaluando el tipo 
+
+console.log(e)
+console.log(w)
